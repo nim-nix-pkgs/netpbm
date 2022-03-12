cea68a4b1d57d4e95fda6e83d5444a84621cb26a
hugosenari <hugosenari@gmail.com>
2022-03-12 04:10:58 -0300

    chore: re index netpbm

---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''Wrapper for libnetpbm'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-netpbm-master.flake = false;
+  inputs.src-netpbm-master.owner = "barcharcraz";
+  inputs.src-netpbm-master.ref   = "refs/heads/master";
+  inputs.src-netpbm-master.repo  = "nim-netpbm";
+  inputs.src-netpbm-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-netpbm-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-netpbm-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
