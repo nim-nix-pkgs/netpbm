a248866c8ef74b2400e8918e285b9893d70b7770
hugosenari <hugosenari@gmail.com>
2022-03-27 21:52:39 -0300

    chore: re index netpbm

--- master/flake.nix
+++ master/flake.nix
@@ -11,7 +11,6 @@
   inputs.src-netpbm-master.ref   = "refs/heads/master";
   inputs.src-netpbm-master.owner = "barcharcraz";
   inputs.src-netpbm-master.repo  = "nim-netpbm";
-  inputs.src-netpbm-master.dir   = "";
   inputs.src-netpbm-master.type  = "github";
   
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
